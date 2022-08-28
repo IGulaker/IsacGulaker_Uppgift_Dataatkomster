9a7bc00e214486b015b3acad13c26cba914282d8
IGulaker <ig.pro@hotmail.com>
2022-08-28 20:47:02 +0200

    Added OrderEntity

--- Data/Entities/OrderEntity.cs
+++ Data/Entities/OrderEntity.cs
@@ -1,6 +1,47 @@
-﻿namespace IsacGulaker_Uppgift_Dataatkomster.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IsacGulaker_Uppgift_Dataatkomster.Data.Entities
 {
     public class OrderEntity
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ProductName { get; set; } = null!;
+
+        [Required]
+        [StringLength(13)]
+        public string ProductEAN_13 { get; set; } = null!;
+
+        [Required]
+        [Column(TypeName = "money")]
+        public decimal ProductPrice { get; set; }
+
+        [Required]
+        public string CustomerFirstName { get; set; } = null!;
+
+        [Required]
+        public string CustomerLastName { get; set; } = null!;
+
+        [Required]
+        public string City { get; set; } = null!;
+
+        [Required]
+        public string PostalCode { get; set; } = null!;
+
+        [Required]
+        public string StreetName { get; set; } = null!;
+        public string? ResidenceNumber { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public string OrderStatus { get; set; } = null!;
     }
 }
