a6a38c6ae622668472c2aecfd57f508c4e4f3f5d
IGulaker <ig.pro@hotmail.com>
2022-08-28 23:54:05 +0200

    Create OrdersController.cs

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IsacGulaker_Uppgift_Dataatkomster.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+    }
+}
