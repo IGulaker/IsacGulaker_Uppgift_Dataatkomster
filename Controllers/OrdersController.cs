6170c189d7974c1144d5eb3e2bf0e0e2e3aae9f4
IGulaker <ig.pro@hotmail.com>
2022-08-28 23:58:58 +0200

    Updated OrderController

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IsacGulaker_Uppgift_Dataatkomster.Models.Order;
+using IsacGulaker_Uppgift_Dataatkomster.Services.Order;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IsacGulaker_Uppgift_Dataatkomster.Controllers
@@ -7,5 +9,41 @@ namespace IsacGulaker_Uppgift_Dataatkomster.Controllers
     [ApiController]
     public class OrdersController : ControllerBase
     {
+        private readonly IOrderManager _orderManager;
+
+        public OrdersController(IOrderManager orderManager)
+        {
+            _orderManager = orderManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostOrderAsync(CreateOrderModel model)
+        {
+            return await _orderManager.CreateOrderAsync(model);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetOrderAsync(int id)
+        {
+            return await _orderManager.ReadOrderAsync(id);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrdersAsync()
+        {
+            return new OkObjectResult(await _orderManager.ReadAllOrdersAsync());
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> PutOrderAsync(int id, UpdateOrderModel model)
+        {
+            return await _orderManager.UpdateOrderAsync(id, model);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteOrderAsync(int id)
+        {
+            return await _orderManager.DeleteOrderAsync(id);
+        }
     }
 }
---
+++ Services/Order/IOrderManager.cs
@@ -0,0 +1,6 @@
+﻿namespace IsacGulaker_Uppgift_Dataatkomster.Services.Order
+{
+    public interface IOrderManager
+    {
+    }
+}
