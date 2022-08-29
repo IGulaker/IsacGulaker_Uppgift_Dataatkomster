7ea23e0aeb70729275c70e9880c62be3d2165ba8
IGulaker <ig.pro@hotmail.com>
2022-08-29 09:32:06 +0200

    PolishedSignUpPage
    Fun fact, varit aktiv på detta projekt i 25h nu.

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,7 +1,7 @@
 function submitNewUser(e){
     e.preventDefault()
 
-    let json = JSON.stringify(
+    let SignUpFormJson = JSON.stringify(
         {
             newUserFirstName: e.target['firstName'].value,
             newUserLastname: e.target['lastName'].value,
@@ -15,15 +15,42 @@ function submitNewUser(e){
                 newAddressResidenceNumber: e.target['residenceNumber'].value
             },
             newUserPassword: e.target['password'].value
-    })
+    });
 
     fetch('https://localhost:7285/api/Authentication/SignUp?keY=e2a228d1-95a9-4ade-8eab-5c14f5a24573',{
         method: 'post',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: json
-    })
+        body: SignUpFormJson
+    });
+}
 
-    console.log(json)
+function attemptLogin(e){
+    e.preventDefault();
+
+    let signInFormJson = JSON.stringify(
+        {
+            email: e.target['emailAddress'].value,
+            password: e.target['password'].value
+    });
+
+    fetch('https://localhost:7285/api/Authentication/SignIn?keY=e2a228d1-95a9-4ade-8eab-5c14f5a24573',{
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: signInFormJson
+    });
+}
+
+function optionalInputChange(e){
+
+    if(e.target.value == ""){
+        e.target.style.fontStyle = "italic";
+    }
+    else{
+        e.target.style.fontStyle = "normal";
+    }
 }
